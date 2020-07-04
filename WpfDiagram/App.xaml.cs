f3c6bf9dc816736556afd9c1ca1a77f679c681ca
hachiilcane <hachi.il.cane@gmail.com>
2020-07-05 03:55:56 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfDiagram/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using WpfDiagram.Views;
+using System.Windows;
+
+namespace WpfDiagram
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
