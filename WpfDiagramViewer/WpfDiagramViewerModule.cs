ea31a101c2199774fce9fef761bafcfc3fac9a18
hachiilcane <hachi.il.cane@gmail.com>
2020-07-05 04:05:25 +0900

    Create a new project: Prism Mocule (.NET core) by Prism Template Pack.

---
+++ WpfDiagramViewer/WpfDiagramViewerModule.cs
@@ -0,0 +1,20 @@
+﻿using WpfDiagramViewer.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace WpfDiagramViewer
+{
+    public class WpfDiagramViewerModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
