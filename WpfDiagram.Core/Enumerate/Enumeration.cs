2b7ae051ebdb10c9f5d883ef4d04dc1e0d3a45dd
hachiilcane <hachi.il.cane@gmail.com>
2020-07-11 18:51:00 +0900

    Add Enumeration base class which can be used instead of  enum types. It's very useful.
    See
    https://docs.microsoft.com/ja-jp/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/enumeration-classes-over-enum-types

---
+++ WpfDiagram.Core/Enumerate/Enumeration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace WpfDiagram.Core.Enumerate
+{
+    public abstract class Enumeration : IComparable
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public |
+                                             BindingFlags.Static |
+                                             BindingFlags.DeclaredOnly);
+
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+
+            if (otherValue == null)
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public int CompareTo(object other)
+        {
+            return Id.CompareTo(((Enumeration)other).Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static T FromId<T>(int id) where T : Enumeration
+        {
+            var matchingItem = Parse<T, int>(id, "Id", item => item.Id == id);
+            return matchingItem;
+        }
+
+        public static T FromName<T>(string name) where T : Enumeration
+        {
+            var matchingItem = Parse<T, string>(name, "Name", item => item.Name == name);
+            return matchingItem;
+        }
+
+        private static T Parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration
+        {
+            var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+                throw new InvalidOperationException($"'{value}' is not a valid {description} in {typeof(T)}");
+
+            return matchingItem;
+        }
+    }
+}
