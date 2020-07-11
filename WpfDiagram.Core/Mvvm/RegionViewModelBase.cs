122db23cc75d091b22ac708849d1191f33805fd2
hachiilcane <hachi.il.cane@gmail.com>
2020-07-11 16:15:49 +0900

    Add ViewModelBase class and RegionViewModelBase class.

---
+++ WpfDiagram.Core/Mvvm/RegionViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfDiagram.Core.Mvvm
+{
+    public class RegionViewModelBase : ViewModelBase, INavigationAware, IConfirmNavigationRequest
+    {
+        protected IRegionManager RegionManager { get; private set; }
+
+        public RegionViewModelBase(IRegionManager regionManager)
+        {
+            RegionManager = regionManager;
+        }
+
+        public virtual void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback)
+        {
+            continuationCallback(true);
+        }
+
+        public virtual bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+
+        }
+
+        public virtual void OnNavigatedTo(NavigationContext navigationContext)
+        {
+
+        }
+    }
+}
---
+++ WpfDiagram.Core/Mvvm/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfDiagram.Core.Mvvm
+{
+    public abstract class ViewModelBase : BindableBase, IDestructible
+    {
+        protected ViewModelBase()
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
